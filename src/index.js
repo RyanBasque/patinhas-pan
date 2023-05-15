3c579e1df7d177e5ecc4d631f13631142f4a0071
Ryan Basque <ryan.basque@trademaster.com.br>
2023-05-15 15:11:44 -0300

    feat: header animations

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const headerBtn = document.querySelector("#menu-btn");
 
 headerBtn.addEventListener("click", () => {
-  document.querySelector("#nav-icon").classList.toggle("open");
+  document.querySelector("#nav-icon").classList.toggle("nav-icon-open");
+  document.querySelector("#header-menu").classList.toggle("header-menu-open");
 });
