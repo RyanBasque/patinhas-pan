927502b01956bff74a56c629969e06747bd7c111
Ryan Basque <ryan.basque@trademaster.com.br>
2023-05-16 09:55:50 -0300

    feat: create header animations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-const headerBtn = document.querySelector("#menu-btn");
+const headerBtn = document.querySelector("#menu-btn-mobile");
 
 headerBtn.addEventListener("click", () => {
   document.querySelector("#nav-icon").classList.toggle("nav-icon-open");
-  document.querySelector("#header-menu").classList.toggle("header-menu-open");
+  document
+    .querySelector("#header-menu-mobile")
+    .classList.toggle("header-menu-open");
 });
